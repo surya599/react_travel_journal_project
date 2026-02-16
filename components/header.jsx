590c3f8d583e45db49f3a333db35d17b619143ed
surya599 <suryabhuvaneshwarreddy@gmail.com>
2026-02-16 20:20:08 +0530

    wriiten code for header part

---
+++ components/header.jsx
@@ -0,0 +1,9 @@
+export default function Header()
+{
+    return(
+        <header>
+            <img src="../Earth_globe.png" />
+            <h1>My Travel Journal.</h1>
+        </header>
+    )
+}
